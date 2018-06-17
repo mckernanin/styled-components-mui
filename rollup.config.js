bfff9dc1dddd38cf61ee6cf39c2083f1df82140e
Kevin McKernan <kevin@mckernan.in>
2018-06-17 01:10:47 -0600

    add examples, contributors

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,23 @@
 import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/index.js',
-    format: 'cjs',
+    format: 'es',
   },
+  exports: 'named',
   plugins: [
     resolve({
       customResolveOptions: {
         moduleDirectory: 'node_modules',
       },
+    }),
+    babel({
+      babelrc: false,
+      presets: ['env', 'flow'],
+      plugins: ['transform-class-properties'],
     }),
   ],
   external: ['styled-components'],
